da2f80a6ff4da51859bda93cecfecf426a4d2bf1
Rui Gomes <rui-miguel.gomes@capgemini.com>
2025-01-28 15:47:57 +0000

    update

--- backend/main.py
+++ backend/main.py
@@ -93,6 +93,8 @@ async def listar_reunioes(cliente_id: int):
 # Endpoint para inserir reunião
 @app.post("/reunioes")
 async def inserir_reuniao(reuniao: ReuniaoData):
+    print("teste")
+    print(reuniao)
     print(reuniao.cliente_id)
     conn = get_db_connection()
     cursor = conn.cursor()
